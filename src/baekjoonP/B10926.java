bc91b6cd93238db076cf671d0237b88891171ab0
da2hn <apple7278@naver.com>
2022-07-23 21:20:35 +0900

    10926번 문제

---
+++ src/baekjoonP/B10926.java
@@ -0,0 +1,14 @@
+package baekjoonP;
+
+import java.util.Scanner;
+
+public class B10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+		
+		System.out.println(id+"??!");
+		
+	}
+}
