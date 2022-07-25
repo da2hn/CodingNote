d12208a32c07e8f9397ed36cc27dd4e9762f0f2f
da2hn <apple7278@naver.com>
2022-07-25 23:12:10 +0900

    15596번 문제

---
+++ src/baekjoonP/B15596.java
@@ -0,0 +1,15 @@
+package baekjoonP;
+
+public class B15596 {
+	long sum(int[] a) {
+		long sum = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		
+		
+		return sum;
+	}
+	
+}
