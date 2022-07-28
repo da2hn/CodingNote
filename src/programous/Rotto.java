1c848add0098f7046dbe6a3acf7e4a379b3e4a7f
da2hn <apple7278@naver.com>
2022-07-28 21:33:37 +0900

    Rotto문제

--- src/programous/Rotto.java
+++ src/programous/Rotto.java
@@ -1,5 +1,51 @@
 package programous;
 
-public class Rotto {
+import java.util.Scanner;
 
+public class Rotto {
+	public int[] solution(int[] lottos, int[] win_nums) {
+	      int[] answer = new int[2];
+	      int max = 0;
+	      int min = 0;
+	      
+	      for (int i = 0; i < win_nums.length; i++) {
+	    	  for (int j = 0; j < win_nums.length; j++) {
+	    		  if(lottos[i] == win_nums[j]) {
+	    			  max ++;
+	    			  min ++;
+	    		  }
+	    	  }
+	      }
+	      
+	      for (int i = 0; i < lottos.length; i++) {
+	    	  if(lottos[i] == 0) {
+	    		  max++;
+	    	  }
+	      }
+	      
+	      
+	      answer[0] = max;
+	      answer[1] = min;
+	      
+	      for (int i = 0; i < answer.length; i++) {
+	    	  switch(answer[i]) {
+	    	  	case 6: answer[i] = 1;
+	    	  		break;
+	    	  	case 5: answer[i] = 2;
+	    	  		break;
+	    	  	case 4: answer[i] = 3;
+	    	  		break;
+	    	  	case 3: answer[i] = 4;
+	    	  		break;
+	    	  	case 2: answer[i] = 5;
+	    	  		break;
+	    	  	default : answer[i] = 6;
+	    	  		break;
+	    	  }
+	      }
+	      
+	      
+	      return answer;
+	  }
+	
 }
